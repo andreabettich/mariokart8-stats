bf02c8c6f74da980d9bb4349583523669f063513
Andrea Bettich <kickmich@gmail.com>
2015-04-19 02:57:13 +0200

    Added JUnit

---
+++ app/src/test/java/ch/masterdrifter/mariokart8stats/JsonTest.java
@@ -0,0 +1,36 @@
+package ch.masterdrifter.mariokart8stats;
+
+import junit.framework.TestCase;
+
+/**
+ * @author Andrea Bettich
+ */
+public class JsonTest extends TestCase {
+    /**
+     * @param name
+     */
+    public JsonTest(String name) {
+        super(name);
+    }
+
+    /* (non-Javadoc)
+     * @see junit.framework.TestCase#setUp()
+     */
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    /* (non-Javadoc)
+     * @see junit.framework.TestCase#tearDown()
+     */
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    /**
+     * Test something
+     */
+    public final void testSomething() {
+        assertEquals(1,1);
+    }
+}
